58942b02c9c6d035c9f4a0bed773dec3d769e47e
Hashim Iqbal <hashimravian007@gmail.com>
2024-07-04 23:50:49 -0600

    Rename app name

--- rsi_project/settings.py
+++ rsi_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rsi_backend',
+    'api',
     'rest_framework',
     'corsheaders',
 ]
